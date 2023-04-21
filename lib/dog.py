cdfa2a4cc599e2cff897fb35315b34d84d9b1c69
Yael Harosh <yaelharosh01@gmail.com>
2023-04-21 09:37:34 -0400

    pass

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,23 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).where(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).where(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).where(Dog.name == name and Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    return session.query(Dog).where(Dog.id == dog.id).update({Dog.breed: breed})
